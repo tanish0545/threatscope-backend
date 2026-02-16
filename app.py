b191c31a33dcdfed297fb1a2438438c4cfbf279a
Upgrade User <user@example.com>
2026-02-17 00:31:10 +0530

    Fix Railway database URL

--- app.py
+++ app.py
@@ -45,6 +45,11 @@ app.config['SESSION_USE_SIGNER'] = True
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 app.config['SESSION_COOKIE_SECURE'] = True  # ✅ True in production (HTTPS only)
 
+
+database_url = os.getenv("DATABASE_URL")
+
+if database_url.startswith("mysql://"):
+    database_url = database_url.replace("mysql://", "mysql+pymysql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
